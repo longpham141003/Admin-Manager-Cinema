b818ce8b6c519b8ec8a0307cd2151f6bf58c189d
longpham141003 <133594334+longpham141003@users.noreply.github.com>
2024-11-21 10:40:47 +0700

    booking.routes.js

--- server/routes/ticket/booking.routes.js
+++ server/routes/ticket/booking.routes.js
@@ -1,25 +1,14 @@
 import express from 'express';
 import { auth, authorize } from '../../middlewares/auth.js';
-import * as bookingController from '../../controllers/ticket/booking.controller.js'; // Đổi tên đường dẫn
+import * as bookingController from '../../controllers/ticket/booking.controller.js';
 
 const router = express.Router();
 
-// Tạo đặt chỗ mới
 router.post('/', auth, bookingController.createBooking);
-
-// Lấy tất cả đặt chỗ
-router.get('/', auth, bookingController.getAllBookingsWithDetails); // Sửa ở đây
-
-// Lấy đặt chỗ theo ID
-router.get('/:id', auth, bookingController.getBookingById); // Đổi thành getBookingById
-
-// Cập nhật trạng thái đặt chỗ
+router.get('/', auth, bookingController.getAllBookingsWithDetails);
+router.get('/:id', auth, bookingController.getBookingById);
 router.put('/:id', auth, bookingController.updateBookingStatus);
-
-// Xóa đặt chỗ
 router.delete('/:id', auth, bookingController.deleteBooking);
-
-// Route để xóa tất cả đặt chỗ
 router.delete('/', auth, authorize(['admin']), bookingController.deleteAllBookings);
 
 export default router;
