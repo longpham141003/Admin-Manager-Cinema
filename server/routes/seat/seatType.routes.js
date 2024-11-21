a74a84888b4ea0be2ed6d68a72b968de3cc4e777
longpham141003 <133594334+longpham141003@users.noreply.github.com>
2024-11-21 10:38:22 +0700

    seatType.routes.js

--- server/routes/seat/seatType.routes.js
+++ server/routes/seat/seatType.routes.js
@@ -4,13 +4,8 @@ import { auth, authorize } from '../../middlewares/auth.js';
 
 const router = express.Router();
 
-// Route để tạo loại ghế
 router.post('/', auth, authorize('admin'), createSeatTypes);
-
-// Route để lấy tất cả loại ghế
 router.get('/', auth, authorize('admin'), getAllSeatTypes);
-
-// Route để xóa tất cả loại ghế
 router.delete('/', auth, authorize('admin'), deleteAllSeatTypes);
 
 export default router;
